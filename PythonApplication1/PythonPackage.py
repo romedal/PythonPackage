e79e20d7348486da9df2ee09d5fb856a187be5d7
Sergii Brodeskur <romedal7@gmail.com>
2019-11-16 03:10:50 +0200

    AfterInitial commit

---
+++ PythonApplication1/PythonPackage.py
@@ -0,0 +1,9 @@
+import Animals
+
+# Create an object of Mammals class & call a method of it
+myMammal = Animals.Mammals()
+myMammal.printMembers()
+ 
+# Create an object of Birds class & call a method of it
+myBird = Animals.Birds()
+myBird.printMembers()
